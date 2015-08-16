c07ea11b48af2032c364b4d35052cb11b2a64e56
yukku0423 <yukku0423@gmail.com>
2015-08-16 23:50:05 +0900

    rewrite es5 to es6

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ var Sub = require('./sub.js');
 Sub(); // => hello! browser.
 
 var _ = require('lodash');
-var a = _.map([1,2,3,4], function(val){
+var a = _.map([1,2,3,4], (val) => {
   return val * 2
 });
 
